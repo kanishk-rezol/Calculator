55467b0ed1119c21119ceb063df4715d8d1d4f40
kanishk-rezol <kanishksriram007@gmail.com>
2024-07-03 08:43:32 +0530

    1st commit

---
+++ cal.js
@@ -0,0 +1,14 @@
+function functionDisplay(value) {
+    document.getElementById('display').value += value;
+}
+function clearDisplay() {
+    document.getElementById('display').value = '';
+}
+function result() {
+    const display = document.getElementById('display');
+    try {
+        display.value = eval(display.value);
+    } catch (e) {
+        display.value = 'Error';
+    }
+}
